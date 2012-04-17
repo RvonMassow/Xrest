b6fa783628510eb6476f68455828c03b640c5390
Robert von Massow <rvm@informatik.uni-kiel.de>
2012-04-17 10:25:31 +0200

    added generated class file, added documentation

---
+++ org.eclipse.xtext.example.domainmodel/src-gen/org/eclipse/xtext/example/domainmodel/domainmodel/Generate.java
@@ -0,0 +1,21 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ */
+package org.eclipse.xtext.example.domainmodel.domainmodel;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Generate</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see org.eclipse.xtext.example.domainmodel.domainmodel.DomainmodelPackage#getGenerate()
+ * @model
+ * @generated
+ */
+public interface Generate extends Operation
+{
+} // Generate
---
+++ org.eclipse.xtext.example.domainmodel/src-gen/org/eclipse/xtext/example/domainmodel/domainmodel/impl/GenerateImpl.java
@@ -0,0 +1,45 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ */
+package org.eclipse.xtext.example.domainmodel.domainmodel.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.xtext.example.domainmodel.domainmodel.DomainmodelPackage;
+import org.eclipse.xtext.example.domainmodel.domainmodel.Generate;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Generate</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class GenerateImpl extends OperationImpl implements Generate
+{
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected GenerateImpl()
+  {
+    super();
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  protected EClass eStaticClass()
+  {
+    return DomainmodelPackage.Literals.GENERATE;
+  }
+
+} //GenerateImpl
